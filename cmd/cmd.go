2f19dfbe94d4cce854622e875f32c9b512ee1193
Jascha <jaschadub@gmail.com>
2024-09-30 13:00:15 -0700

    Update cmd.go
    path

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -7,9 +7,9 @@ import (
 	"os"
 	"strings"
 
-	"github.com/dutchcoders/transfer.sh/server/storage"
+	"github.com/jaschadub/transfer.sh/server/storage"
 
-	"github.com/dutchcoders/transfer.sh/server"
+	"github.com/jaschadub/transfer.sh/server"
 	"github.com/fatih/color"
 	"github.com/urfave/cli/v2"
 	"google.golang.org/api/googleapi"
